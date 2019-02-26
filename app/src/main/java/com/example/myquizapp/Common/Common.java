8ece721c5969f5587947ea21c9fbec7f113b1006
Matan Ladani <matanladani@gmail.com>
2019-02-26 20:56:17 +0200

    Initial commit

---
+++ app/src/main/java/com/example/myquizapp/Common/Common.java
@@ -0,0 +1,13 @@
+package com.example.myquizapp.Common;
+
+import com.example.myquizapp.Model.Question;
+import com.example.myquizapp.Model.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Common {
+    public static String categoryId,categoryName;
+    public static User currentUser;
+    public static List<Question> questionList = new ArrayList<>();
+}
---
+++ app/src/main/java/com/example/myquizapp/Interface/RankingCallBack.java
@@ -0,0 +1,5 @@
+package com.example.myquizapp.Interface;
+
+public interface RankingCallBack<T>{
+    void callBack(T ranking);
+}
